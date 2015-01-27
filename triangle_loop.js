b7826627dca372698db827b493104ff8af55e483
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-27 12:05:04 -0500

    Solved fizzbuzz

---
+++ triangle_loop.js
@@ -0,0 +1,15 @@
+// Looping a triangle
+//
+// Write a loop that makes seven calls to console.log to output the following triangle:
+//
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+
+for (var pound = "#"; pound.length <= 7; pound = pound + "#")
+console.log(pound);
