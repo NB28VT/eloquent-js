d7d2bfa07fd425f34fee90de37107ffea70e006f
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-12 16:36:09 -0500

    Completed sum of range

--- sum_of_range.js
+++ sum_of_range.js
@@ -32,8 +32,34 @@ console.log(sumRange(myRange(1,10)));
 
 
 // As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
-//
-// // Your code here.
+
+mySteppedRange = function(start, end, step){
+  if (step == undefined) {
+    step = 1;
+  }
+
+  var array = [];
+
+  if (step < 0) {
+    for(var num = end; num >= start; num += step){
+      array.push(num);
+    }
+  }else{
+    for(var num = start; num <= end; num += step){
+      array.push(num);
+    }
+  }
+  return array;
+};
+
+console.log(sumRange(mySteppedRange(1,10)));
+
+
+
+
+
+
+
 //
 // console.log(sum(range(1, 10)));
 // // → 55
