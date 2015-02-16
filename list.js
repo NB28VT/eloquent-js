f623d516e8b590b87e85af789788e2a6ed4453a8
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-16 16:08:43 -0500

    Solved nth helper for linked list

--- list.js
+++ list.js
@@ -63,5 +63,11 @@ var prepend = function(element, list){
 
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
+
+var nth = function(list, index){
+  var array = listToArray(list);
+  return array[index];
+}
+
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
