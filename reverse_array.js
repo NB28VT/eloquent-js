067016fece3691ae2e64caa967ba38987d6c0337
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-16 15:13:19 -0500

    Solved all reverse array problems

--- reverse_array.js
+++ reverse_array.js
@@ -26,8 +26,24 @@ var reverseArray = function(array){
 console.log(reverseArray(["A", "B", "C"]));
 
 
+// The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+// //
+// hint
+// The trick is to swap the first and last elements, then the second and second-to-last, and so on. You can do this by looping over half the length of the array (use Math.floor to round down—you don’t need to touch the middle element in an array with an odd length) and swapping the element at position i with the one at position array.length - 1 - i. You can use a local variable to briefly hold on to one of the elements, overwrite that one with its mirror image, and then put the value from the local variable in the place where the mirror image used to be.
 
 
+var reverseArrayInPlace = function(array){
+  for(i = 0; i < Math.floor(array.length/2); i++){
+    var value = array[i];
+      array[i] = array[(array.length - 1 - i)];
+      array[(array.length - 1 - i)] = value;
+  }
+  return array;
+}
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
 
 
 
