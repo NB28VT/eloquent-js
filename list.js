30ce638f972cdc9bdd650a7a7f1e83dfd806dce1
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-16 16:03:46 -0500

    Prepend linked list

--- list.js
+++ list.js
@@ -61,7 +61,6 @@ var prepend = function(element, list){
   return list;
 }
 
-
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
