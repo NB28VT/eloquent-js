8ecce2ce5a23d6217bfb493fbdac8e7eb9f60138
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-04-01 11:42:23 -0400

    World object in electronic life

--- electronic_life.js
+++ electronic_life.js
@@ -1,3 +1,5 @@
+
+// Vectors
 function Vector(x, y){
   this.x = x;
   this.y = y;
@@ -8,6 +10,8 @@ Vector.prototype.plus = function(other){
 };
 
 
+
+// Grid
 function Grid(width, height){
   this.space = new Array(width * height);
   this.width = width;
@@ -26,9 +30,84 @@ Grid.prototype.set = function(vector, value) {
   this.space[vector.x + this.width * vector.y] = value;
 };
 
+// Tests
+// var grid = new Grid(5,5);
+// console.log(grid.get(new Vector(1,1)));
+//
+// grid.set(new Vector(1,1), "X");
+// console.log(grid.get(new Vector(1,1)));
 
-var grid = new Grid(5,5);
-console.log(grid.get(new Vector(1,1)));
+var directions = {
+  "n": new Vector(0, -1),
+  "ne": new Vector(1, -1),
+  "e": new Vector(1, 0),
+  "se": new Vector(1, 1),
+  "sw": new Vector(-1, 1),
+  "w": new Vector(-1, 0),
+  "nw": new Vector(-1, -1)
+};
 
-grid.set(new Vector(1,1), "X");
-console.log(grid.get(new Vector(1,1)));
+function randomElement(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var directionNames = "n ne e se s sw w nw ".split(" ");
+
+// Bouncing critter
+
+function BouncingCritter(){
+  this.direction = randomElement(directionNames);
+};
+
+BouncingCritter.prototype.act = function(view){
+  if (view.look(this.direction) != " ")
+    this.direction = view.find(" ") || "s";
+  return { type: "move", direction: this.direction};
+};
+
+
+// Char elements
+
+function elementFromChar(legend, ch){
+  if (ch == " ")
+    return null;
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+// World object
+
+function World(map, legend){
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function(line, y){
+    for (var x = 0; x < line.length; x++)
+      grid.set(new Vector(x,y),
+        elementFromChar(legend, line[x]));
+  });
+}
+
+function charFromElement(element) {
+  if (element == null)
+    return " ";
+  else
+    return element.originChar;
+}
+
+World.prototype.toString = function(){
+  var output = "";
+  for (var y = 0; y < this.grid.height; y++){
+    for (var x = 0; x < this.grid.width; x++){
+      var element = this.grid.get(new Vector(x,y));
+      output += charFromElement(element);
+    }
+    output += "/n";
+  }
+  return output;
+};
+
+
+// Wall object
